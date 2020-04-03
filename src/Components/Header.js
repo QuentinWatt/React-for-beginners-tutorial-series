0c89d6105ea114b8ad22c0bc43911bde805c41f0
Quentin <quentin@Quentins-iMac.local>
2020-04-03 14:35:20 +0200

    Episode 5 opening and closing the menu with state

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 function Header(){
     return (
-        <header className="border-b font-bold p-3">
-            AppName
+        <header className="border-b p-3 flex justify-between items-center">
+            <span className="font-bold">
+                AppName
+            </span>
+
+            <Navigation />
         </header>
     )
 }
---
+++ src/Components/Navigation.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import {useTransition, animated} from 'react-spring'
+
+function Navigation(){
+    const [showMenu, setShowMenu] = useState(false)
+
+    let menu
+    let menuMask
+
+    if(showMenu){
+        menu = 
+        <div
+            className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+        >
+            The menu
+        </div>
+
+        menuMask = 
+        <div
+            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+            onClick={() => setShowMenu(false)}
+        >
+
+        </div>
+    }
+
+    return (
+        <nav>
+            <span className="text-xl">
+                <FontAwesomeIcon 
+                    icon={faBars}
+                    onClick={() => setShowMenu(!showMenu)}
+                />
+            </span>
+
+            {menuMask}
+
+            { menu }
+        </nav>
+    )
+}
+
+export default Navigation
