0d41258795aa5c8e94a3955d8b6df7efaf31b1c8
Quentin <quentin@Quentins-iMac.local>
2020-04-03 14:36:54 +0200

    Episode 5 opening and closing the menu with state

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import {useTransition, animated} from 'react-spring'
 
 function Navigation(){
     const [showMenu, setShowMenu] = useState(false)
