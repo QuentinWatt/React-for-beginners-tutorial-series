5d174570448674579b749ef7b22814c48fc35b15
Quentin <quentin@Quentins-iMac.local>
2020-04-03 13:06:57 +0200

    Episode 4 - create a basic app part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import './App.css';
 import HelloWorld from './Components/HelloWorld'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div>
+      <Header />
+
       <HelloWorld name="Quentin"/>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(){
+    return (
+        <header className="border-b font-bold p-3">
+            AppName
+        </header>
+    )
+}
+
+export default Header
