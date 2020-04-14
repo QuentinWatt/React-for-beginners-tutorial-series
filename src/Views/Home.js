d62386a24de361169e166c48470b941dc8803701
Quentin <quentin@Quentins-iMac.local>
2020-04-14 16:03:48 +0200

    tutorial starting point

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -4,7 +4,9 @@ import HelloWorld from '../Components/HelloWorld'
 function Home(){
     return (
         <div>
-            <h1 className="font-bold text-2xl mb-3">This is the home page</h1>
+            <h1 className="font-bold text-2xl mb-3">
+                This is the home page
+            </h1>
 
             <HelloWorld name="Quentin" />
         </div>
